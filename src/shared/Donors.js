7fcb3f4f0b0461478e29b2fc53c78f8f70e7783a
Mark Alindogan <marleoali_777@yahoo.com>
2021-03-12 21:51:50 -0600

    folders and Donors Array

---
+++ src/shared/Donors.js
@@ -0,0 +1,21 @@
+export const DONORS = [
+{
+    name: "Mikala",
+    hairColor: "brunette",
+    height: "64",
+    age: "27"
+},
+{
+    name: "Jessica",
+    hairColor: "ginger",
+    height: "62",
+    age: "24"
+},
+{
+    name: "Tammy Ann",
+    hairColor: "brunette",
+    height: "59",
+    age: "31"
+}
+
+]
