43b46da6ad3c031a8b6165060567ba2e8723dec4
Mark Alindogan <marleoali_777@yahoo.com>
2021-03-17 20:08:12 -0500

    getting the site working

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import Footer from './FooterComponent';
+import { DONORS } from '../shared/Donors';
+import { render } from 'react-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+class Main extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            donors: DONORS
+        };
+    }
+
+    render() {
+        return(
+            <div>
+                <Header />
+
+            </div>
+        );
+    }
+}
+
+export default Main;
