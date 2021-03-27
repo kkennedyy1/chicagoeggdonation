e55a6bebd04a59da26a2a9bb85cce2fbd72877d0
Kevin Kennedy <kkennedyy1@gmail.com>
2021-03-26 19:40:07 -0500

    Modal added to the clicked donor from donor list

--- src/Components/FindADonorComponent.js
+++ src/Components/FindADonorComponent.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardText, CardImg, CardImgOverlay, CardTitle  } from 'reactstrap';
+import { Button, Card, CardBody, CardText, CardImg, CardImgOverlay, CardTitle, Modal, ModalBody  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class FindADonor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedDonor: null
+            selectedDonor: null,
+            isModalOpen: false
         };
+       
+        this.toggleModal = this.toggleModal.bind(this);
     }
 
     onDonorSelect(donor) {
         this.setState({selectedDonor: donor});
     }
 
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
     renderSelectedDonor(donor) {
         if (donor) {
             return (
-                <Card>                    
-                    <CardBody>
-                        <CardTitle><h4>{donor.name}, {donor.age}</h4></CardTitle>
-                        <CardImg top src={donor.image} alt={donor.name} />
-                        <CardText><h3>{donor.description}</h3></CardText>
-                    </CardBody>
-                </Card>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalBody>
+                        <Card>                    
+                            <CardBody>
+                                <CardTitle><h4>{donor.name}, {donor.age}</h4></CardTitle>
+                                <CardImg top src={donor.image} alt={donor.name} />
+                                <CardText><h3>{donor.description}</h3></CardText>
+                            </CardBody>
+                        </Card>
+                    </ModalBody>
+                </Modal>
             );
         }
         return <div />;
@@ -32,12 +45,14 @@ class FindADonor extends Component {
         const findadonor = this.props.donor.map(donor => {
             return (
                 <div key={donor.id} className="col-md-3 m-1">
-                    <Card onClick={() => this.onDonorSelect(donor)}>
-                        <CardImg width="100%" height="auto" src={donor.image} alt={donor.name} />
-                        <CardImgOverlay>
-                            <CardTitle><h4 className="cardtitle">{donor.name}, {donor.age}</h4></CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                    <Button className="btn-donor" onClick={this.toggleModal}>
+                        <Card onClick={() => this.onDonorSelect(donor)}>
+                            <CardImg width="100%" height="auto" src={donor.image} alt={donor.name} />
+                            <CardImgOverlay>
+                                <CardTitle><h4 className="cardtitle">{donor.name}, {donor.age}</h4></CardTitle>
+                            </CardImgOverlay>
+                        </Card>
+                    </ Button>
                 </div>
             );
         });
